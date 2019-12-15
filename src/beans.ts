a0e0e8cd12a55510b0f32307d78bdf784621904c
zjf <zjffun@gmail.com>
2019-12-15 22:07:00 +0800

    fix ts

--- src/beans.ts
+++ src/beans.ts
@@ -1,107 +1,186 @@
-var pangu = require('pangu');
+var pangu = require("pangu");
 
 export class Bean {
-    constructor(obj ? : any) {
-        Object.assign(this, obj);
-    }
+  constructor(obj?: any) {
+    Object.assign(this, obj);
+  }
 
-    cloneWithDollarPrefix() {
-        let obj = Object.assign(Object.create(this.constructor.prototype), this);
-        for (const key in obj) {
-            if (obj.hasOwnProperty(key)) {
-                obj["$" + key] = obj[key]
-                delete(obj[key]);
-            }
-        }
-        return obj;
+  cloneWithDollarPrefix() {
+    let obj = Object.assign(Object.create(this.constructor.prototype), this);
+    for (const key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        obj["$" + key] = obj[key];
+        delete obj[key];
+      }
     }
+    return obj;
+  }
 
-    removeEmpty() {
-        let obj = Object.assign(Object.create(this.constructor.prototype), this);
-        for (const key in obj) {
-            !obj[key] && delete(obj[key]);
-        }
-        return obj;
+  removeEmpty() {
+    let obj = Object.assign(Object.create(this.constructor.prototype), this);
+    for (const key in obj) {
+      !obj[key] && delete obj[key];
     }
+    return obj;
+  }
 }
 export class UserBean extends Bean {
-    user: string = this.user || "";
-    pwd: string = this.pwd || "";
-    appKey: string = this.appKey || "";
-    isCurrent: number = this.isCurrent || 0;
-};
+  user: string;
+  pwd: string;
+  appKey: string;
+  isCurrent: number;
+
+  constructor(obj?: any) {
+    super(obj);
+    this.user = this.user || "";
+    this.pwd = this.pwd || "";
+    this.appKey = this.appKey || "";
+    this.isCurrent = this.isCurrent || 0;
+  }
+}
 export class BlogInfoBean extends Bean {
-    blogid: string = this.blogid || "";
-    url: string = this.url || "";
-    blogName: string = this.blogName || "";
-    blogUser: string = this.blogUser || "";
-    isCurrent: number = this.isCurrent || 0;
-};
+  blogid: string;
+  url: string;
+  blogName: string;
+  blogUser: string;
+  isCurrent: number;
+
+  constructor(obj?: any) {
+    super(obj);
+    this.blogid = this.blogid || "";
+    this.url = this.url || "";
+    this.blogName = this.blogName || "";
+    this.blogUser = this.blogUser || "";
+    this.isCurrent = this.isCurrent || 0;
+  }
+}
 export class PostBean extends Bean {
-    dateCreated: number = this.dateCreated || 0;
-    description: string = this.description || "";
-    title: string = this.title || "";
-    titlePangu: string = pangu.spacing(this.title);
-    categories: string | Array < string > = Object.prototype.toString.call(this.categories) === '[object Array]' ?
-        JSON.stringify(this.categories) : this.categories || "[]";
-    enclosure: string = this.enclosure || "";
-    link: string = this.link || "";
-    permalink: string = this.permalink || "";
-    postid: string = this.postid || "";
-    source: string = this.source || "";
-    userid: string = this.userid || "";
-    mt_allow_comments: string = this.mt_allow_comments || "";
-    mt_allow_pings: string = this.mt_allow_pings || "";
-    mt_convert_breaks: string = this.mt_convert_breaks || "";
-    mt_excerpt: string = this.mt_excerpt || "";
-    mt_keywords: string = this.mt_keywords || "";
-    wp_slug: string = this.wp_slug || "";
-    addtype: string = this.addtype || "";
+  dateCreated: number;
+  description: string;
+  title: string;
+  titlePangu: string;
+  categories: string | Array<string>;
+  enclosure: string;
+  link: string;
+  permalink: string;
+  postid: string;
+  source: string;
+  userid: string;
+  mt_allow_comments: string;
+  mt_allow_pings: string;
+  mt_convert_breaks: string;
+  mt_excerpt: string;
+  mt_keywords: string;
+  wp_slug: string;
+  addtype: string;
+  constructor(obj?: any) {
+    super(obj);
+    this.dateCreated = this.dateCreated || 0;
+    this.description = this.description || "";
+    this.title = this.title || "";
+    this.titlePangu = pangu.spacing(this.title);
+    this.categories =
+      Object.prototype.toString.call(this.categories) === "[object Array]"
+        ? JSON.stringify(this.categories)
+        : "[]";
+    this.enclosure = this.enclosure || "";
+    this.link = this.link || "";
+    this.permalink = this.permalink || "";
+    this.postid = this.postid || "";
+    this.source = this.source || "";
+    this.userid = this.userid || "";
+    this.mt_allow_comments = this.mt_allow_comments || "";
+    this.mt_allow_pings = this.mt_allow_pings || "";
+    this.mt_convert_breaks = this.mt_convert_breaks || "";
+    this.mt_excerpt = this.mt_excerpt || "";
+    this.mt_keywords = this.mt_keywords || "";
+    this.wp_slug = this.wp_slug || "";
+    this.addtype = this.addtype || "";
+  }
+  getCBParas() {
+    let obj = Object.assign(Object.create(this.constructor.prototype), this);
+    obj.dateCreated && delete obj.dateCreated;
+    obj.description || (obj.description = "null");
+    obj.categories = JSON.parse(obj.categories);
+    return obj;
+  }
 
-    getCBParas() {
-        let obj = Object.assign(Object.create(this.constructor.prototype), this);
-        obj.dateCreated && delete(obj.dateCreated);
-        obj.description || (obj.description = "null");
-        obj.categories = JSON.parse(obj.categories);
-        return obj;
-    };
-
-    removeEmpty() {
-        let obj = Object.assign(Object.create(this.constructor.prototype), this);
-        obj.categories === "[]" && delete(obj.categories);
-        for (const key in obj) {
-            !obj[key] && delete(obj[key]);
-        }
-        return obj;
+  removeEmpty() {
+    let obj = Object.assign(Object.create(this.constructor.prototype), this);
+    obj.categories === "[]" && delete obj.categories;
+    for (const key in obj) {
+      !obj[key] && delete obj[key];
     }
-};
+    return obj;
+  }
+}
 export class CategoryInfoBean extends Bean {
-    description: string = this.description || "";
-    htmlUrl: string = this.htmlUrl || "";
-    rssUrl: string = this.rssUrl || "";
-    title: string = this.title || "";
-    categoryid: string = this.categoryid || ""
-};
+  description: string;
+  htmlUrl: string;
+  rssUrl: string;
+  title: string;
+  categoryid: string;
+
+  constructor(obj?: any) {
+    super(obj);
+    this.description = this.description || "";
+    this.htmlUrl = this.htmlUrl || "";
+    this.rssUrl = this.rssUrl || "";
+    this.title = this.title || "";
+    this.categoryid = this.categoryid || "";
+  }
+}
 export class FileDataBean extends Bean {
-    bits: string = this.bits || "";
-    name: string = this.name || "";
-    type: string = this.type || ""
-};
+  bits: string;
+  name: string;
+  type: string;
+
+  constructor(obj?: any) {
+    super(obj);
+    this.bits = this.bits || "";
+    this.name = this.name || "";
+    this.type = this.type || "";
+  }
+}
 export class UrlDataBean extends Bean {
-    url: string = this.url || ""
-};
+  url: string;
+  constructor(obj?: any) {
+    super(obj);
+    this.url = this.url || "";
+  }
+}
 export class WpCategoryBean extends Bean {
-    name: string = this.name || "";
-    slug: string = this.slug || "";
-    parent_id: number = this.parent_id || 0;
-    description: string = this.description || ""
-};
+  name: string;
+  slug: string;
+  parent_id: number;
+  description: string;
+
+  constructor(obj?: any) {
+    super(obj);
+    this.name = this.name || "";
+    this.slug = this.slug || "";
+    this.parent_id = this.parent_id || 0;
+    this.description = this.description || "";
+  }
+}
 export class EnclosureBean extends Bean {
-    length: number = this.length || 0;
-    type: string = this.type || "";
-    url: string = this.url || ""
-};
+  length: number;
+  type: string;
+  url: string;
+
+  constructor(obj?: any) {
+    super(obj);
+    this.length = this.length || 0;
+    this.type = this.type || "";
+    this.url = this.url || "";
+  }
+}
 export class SourceBean extends Bean {
-    name: string = this.name || "";
-    url: string = this.url || ""
-};
+  name: string;
+  url: string;
+  constructor(obj?: any) {
+    super(obj);
+    this.name = this.name || "";
+    this.url = this.url || "";
+  }
+}
